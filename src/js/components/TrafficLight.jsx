06e74941da17d689d4c64db1b0882af354ef7456
Vlad <pacom4n90@gmail.com>
2025-08-11 17:18:15 +0000

    Semaforo Vlad

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const TrafficLight = () => {
+   
+    const [color, setColor] = useState("red");
+
+    
+    useEffect(() => {
+    
+        const colors = ["red", "yellow", "green"];
+        let currentColorIndex = colors.indexOf(color);
+
+        // temporizador que se ejecuta cada 3 segundos.
+        const timer = setInterval(() => {
+            currentColorIndex = (currentColorIndex + 1) % colors.length;
+            setColor(colors[currentColorIndex]);
+        }, 3000);
+
+      
+        return () => clearInterval(timer);
+    }, [color]); 
+
+    
+    const changeColorManual = () => {
+        const colors = ["red", "yellow", "green"];
+        const currentColorIndex = colors.indexOf(color);
+        const nextColorIndex = (currentColorIndex + 1) % colors.length;
+        setColor(colors[nextColorIndex]);
+    };
+
+    return (
+        <div className="traffic-light-container">
+            <div className="traffic-light-pole"></div>
+            <div className="traffic-light-box">
+                {/*  Rojo */}
+                <div
+                    className={`light red-light ${color === "red" ? "glow" : ""}`}
+                ></div>
+                {/*  Amarillo */}
+                <div
+                    className={`light yellow-light ${color === "yellow" ? "glow" : ""}`}
+                ></div>
+                {/*  Verde */}
+                <div
+                    className={`light green-light ${color === "green" ? "glow" : ""}`}
+                ></div>
+            </div>
+
+            <button className="change-color-button" onClick={changeColorManual}>
+                Cambia el color
+            </button>
+        </div>
+    );
+};
+
+export default TrafficLight;
