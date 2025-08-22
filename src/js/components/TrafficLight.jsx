ab243abf94e3e5fe58ea78dd9462c74e730b950c
Vlad <pacom4n90@gmail.com>
2025-08-22 13:32:19 +0000

    Vlad Semaforo v2

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,56 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const TrafficLight = () => {
-   
-    const [color, setColor] = useState("red");
-
-    
-    useEffect(() => {
-    
-        const colors = ["red", "yellow", "green"];
-        let currentColorIndex = colors.indexOf(color);
-
-        // temporizador que se ejecuta cada 3 segundos.
-        const timer = setInterval(() => {
-            currentColorIndex = (currentColorIndex + 1) % colors.length;
-            setColor(colors[currentColorIndex]);
-        }, 3000);
-
-      
-        return () => clearInterval(timer);
-    }, [color]); 
-
-    
-    const changeColorManual = () => {
-        const colors = ["red", "yellow", "green"];
-        const currentColorIndex = colors.indexOf(color);
-        const nextColorIndex = (currentColorIndex + 1) % colors.length;
-        setColor(colors[nextColorIndex]);
-    };
-
-    return (
-        <div className="traffic-light-container">
-            <div className="traffic-light-pole"></div>
-            <div className="traffic-light-box">
  
-                <div
-                    className={`light red-light ${color === "red" ? "glow" : ""}`}
-                ></div>
-    
-                <div
-                    className={`light yellow-light ${color === "yellow" ? "glow" : ""}`}
-                ></div>
-    
-                <div
-                    className={`light green-light ${color === "green" ? "glow" : ""}`}
-                ></div>
-            </div>
+  const [color, setColor] = useState("red");
 
-            <button className="change-color-button" onClick={changeColorManual}>
-                Cambia el color
-            </button>
-        </div>
-    );
+  const changeColorManual = () => {
+    const colors = ["red", "yellow", "green"];
+    const currentColorIndex = colors.indexOf(color);
+    const nextColorIndex = (currentColorIndex + 1) % colors.length;
+    setColor(colors[nextColorIndex]);
+  };
+
+  return (
+    <div className="traffic-light-container">
+      <div className="traffic-light-pole"></div>
+      <div className="traffic-light-box">
+       
+        <div
+          className={`light red-light ${color === "red" ? "glow" : ""}`}
+          onClick={() => setColor("red")}
+        ></div>
+        
+       
+        <div
+          className={`light yellow-light ${color === "yellow" ? "glow" : ""}`}
+          onClick={() => setColor("yellow")}
+        ></div>
+        
+    
+        <div
+          className={`light green-light ${color === "green" ? "glow" : ""}`}
+          onClick={() => setColor("green")}
+        ></div>
+      </div>
+
+      <button className="change-color-button" onClick={changeColorManual}>
+        Cambia el color
+      </button>
+    </div>
+  );
 };
 
 export default TrafficLight;
